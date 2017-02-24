2b448ea32b7bb1e8ee2be0f501e707dde678e824
Max Rupplin - Sr. Scientologist <mearvk@outlook.com>
2017-02-24 13:00:19 -0500

    latest

---
+++ src/apml/compilers/java/roaster/Bloqfileguardian.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apml.compilers.java.roaster;
+
+import java.io.File;
+
+/**
+ *
+ * @author max rupplin
+ */
+public class Bloqfileguardian 
+{
+    public File manifestfile;
+    
+    public File manifestfiledir;    
+    
+    public File sourceoutdir;   
+    
+    public File buildoutdir;
+    
+    public File apmlxmlinputfile;
+    
+    public final String apmlinjarurl = "/home/oem/NetBeansProjects/APML/dist/APML.jar";
+    
+    public final String apmloutjarurl = "/home/oem/Desktop/apml/output/libs/APML.jar";
+    
+    public final String apmlinurl = "/home/oem/NetBeansProjects/APML/src/apml/examples/echoserver/server/echoserver.xml";
+    
+    public final String basedirurl = "/home/oem/Desktop/apml/output/echo/";
+    
+    public final String builddirurl = "build/";
+    
+    public final String srcdirurl = "source/";    
+    
+    public final String tempsrcdirurl = "temp/";
+    
+    public final String manifestdirurl = "manifest/";
+    
+    public final String manifestfileurl = "/home/oem/Desktop/apml/output/manifest/manifest.txt";     
+}
