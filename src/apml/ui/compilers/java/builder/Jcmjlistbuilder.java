7fd0e4d450ced2ed49c743ca4d9f2d42568790f8
Max Rupplin - Sr. Scientologist <mearvk@outlook.com>
2017-02-26 19:01:53 -0500

    latest

--- src/apml/ui/compilers/java/builder/Jcmjlistbuilder.java
+++ src/apml/ui/compilers/java/builder/Jcmjlistbuilder.java
@@ -1,8 +1,13 @@
 package apml.ui.compilers.java.builder;
 
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JPackage;
 import java.io.File;
 import java.util.ArrayList;
+import javax.swing.JMenuItem;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
@@ -38,42 +43,162 @@ public class Jcmjlistbuilder
     
     public ArrayList<JCodeModel> build(String tagname)
     {
+        JCodeModel jcodemodel = new JCodeModel();
+        
         try
         {        
             this.doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(apml);
         
             this.nodes = (NodeList)xpath.evaluate(tagname, this.doc, XPathConstants.NODESET);
             
-            //would prefer to go right to jcm from here
-            xml.getAttribute("setAction");                                                
+            for(int i=0; i<nodes.getLength(); i++)
+            {            
+                this.xml = (Element)nodes.item(i);
+                
+                JPackage jpackage = jcodemodel._package("org.widgets");                
+                
+                JDefinedClass jdefinedclass = jpackage._class("JMenuItem_"+String.format("%1$03d",i));   
+                
+                jdefinedclass._extends(JMenuItem.class);
+                
+                JMethod constructor = jdefinedclass.constructor(JMod.PUBLIC);      
+                
+                if(xml.getAttribute("setAutoscrolls")!=null && xml.getAttribute("setAutoscrolls").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setAutoscrolls("+xml.getAttribute("setAutoscrolls")+");");                 }catch(Exception e){}               
+                }
+                
+                if(xml.getAttribute("setAlignmentY")!=null && xml.getAttribute("setAlignmentY").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setAlignmentY("+xml.getAttribute("setAlignmentY")+");");                   }catch(Exception e){}                
+                }
+                
+                if(xml.getAttribute("setAlignmentX")!=null && xml.getAttribute("setAlignmentX").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setAlignmentX("+xml.getAttribute("setAlignmentX")+");");                   }catch(Exception e){}                
+                }
+                
+                if(xml.getAttribute("setBorderPainted")!=null && xml.getAttribute("setBorderPainted").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setBorderPainted("+xml.getAttribute("setBorderPainted")+");");             }catch(Exception e){}               
+                }
+                                
+                if(xml.getAttribute("setBounds")!=null && xml.getAttribute("setBounds").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setBounds("+xml.getAttribute("setBounds")+");");                           }catch(Exception e){}
+                }
+                                
+                if(xml.getAttribute("setBorder")!=null && xml.getAttribute("setBorder").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setBorder("+xml.getAttribute("setBorder")+");");                           }catch(Exception e){}                    
+                }
+                
+                if(xml.getAttribute("setBackground")!=null && xml.getAttribute("setBackground").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setBackground("+xml.getAttribute("setBackground")+");");                   }catch(Exception e){}                                    
+                }
+                
+                if(xml.getAttribute("setComponentPopupMenu")!=null && xml.getAttribute("setComponentPopupMenu").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setComponentPopupMenu("+xml.getAttribute("setComponentPopupMenu")+");");   }catch(Exception e){}                      
+                }
+                
+                if(xml.getAttribute("setDisabledIcon")!=null && xml.getAttribute("setDisabledIcon").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setDisabledIcon("+xml.getAttribute("setDisabledIcon")+");");               }catch(Exception e){}
+                }
 
-            xml.getAttribute("setBorderpainted");
+                if(xml.getAttribute("setEnabled")!=null && xml.getAttribute("setEnabled").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setEnabled("+xml.getAttribute("setEnabled")+");");                         }catch(Exception e){}  
+                }                
+                
+                if(xml.getAttribute("setForeground")!=null && xml.getAttribute("setForeground").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setForeground("+xml.getAttribute("setForeground")+");");                   }catch(Exception e){} 
+                }
 
-            xml.getAttribute("setDisabledicon");
+                if(xml.getAttribute("setIcon")!=null && xml.getAttribute("setIcon").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setIcon("+xml.getAttribute("setIcon")+")");                                }catch(Exception e){}
+                }
 
-            xml.getAttribute("setEnabled");                
+                if(xml.getAttribute("setLabel")!=null && xml.getAttribute("setLabel").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setLabel("+xml.getAttribute("setLabel")+")");                              }catch(Exception e){}
+                }
+                
+                if(xml.getAttribute("setLayout")!=null && xml.getAttribute("setLayout").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setLayout("+xml.getAttribute("setLayout")+");");                           }catch(Exception e){}
+                }
+                
+                if(xml.getAttribute("setLocation")!=null && xml.getAttribute("setLocation").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setLocation("+xml.getAttribute("setLocation")+");");                       }catch(Exception e){}
+                }
+                
+                if(xml.getAttribute("setMargin")!=null && xml.getAttribute("setMargin").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setMargin("+xml.getAttribute("setMargin")+");");                           }catch(Exception e){}
+                }
+                
+                if(xml.getAttribute("setMinimumSize")!=null && xml.getAttribute("setMinimumSize").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setMinimumSize("+xml.getAttribute("setMinimumSize")+");");                 }catch(Exception e){}
+                }
+                
+                if(xml.getAttribute("setMaximumSize")!=null && xml.getAttribute("setMaximumSize").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setMaximumSize("+xml.getAttribute("setMaximumSize")+");");                 }catch(Exception e){}            
+                }
+                
+                if(xml.getAttribute("setModel")!=null && xml.getAttribute("setModel").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setModel("+xml.getAttribute("setModel")+");");                             }catch(Exception e){}
+                }
+                                
+                if(xml.getAttribute("setName")!=null && xml.getAttribute("setName").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setName("+xml.getAttribute("setName")+");");                               }catch(Exception e){}
+                }
+                
+                if(xml.getAttribute("setPressedIcon")!=null && xml.getAttribute("setPressedIcon").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setPressedIcon("+xml.getAttribute("setPressedIcon")+");");                 }catch(Exception e){}
+                }
+                
+                if(xml.getAttribute("setRolloverIcon")!=null && xml.getAttribute("setRolloverIcon").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setRolloverIcon("+xml.getAttribute("setRolloverIcon")+");");               }catch(Exception e){}                    
+                }
 
-            xml.getAttribute("setIcon");
-
-            xml.getAttribute("setLabel");
-
-            xml.getAttribute("setLayout");
-
-            xml.getAttribute("setMargin");
-
-            xml.getAttribute("setModel");
-
-            xml.getAttribute("setName");                                                
-
-            xml.getAttribute("setPressedIcon");
-
-            xml.getAttribute("setRolloverIcon");                
-
-            xml.getAttribute("setText"); 
+                if(xml.getAttribute("setText")!=null && xml.getAttribute("setText").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setText(\""+xml.getAttribute("setText")+"\");");                           }catch(Exception e){}   
+                }
+                
+                if(xml.getAttribute("setSize")!=null && xml.getAttribute("setSize").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setSize("+xml.getAttribute("setSize")+");");                               }catch(Exception e){}
+                }
+                
+                if(xml.getAttribute("setToolTipText")!=null && xml.getAttribute("setToolTipText").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setToolTipText("+xml.getAttribute("setToolTipText")+");");                 }catch(Exception e){}
+                }
+                
+                if(xml.getAttribute("setVisible")!=null && xml.getAttribute("setVisible").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setVisible("+xml.getAttribute("setVisible")+");");                         }catch(Exception e){}
+                }
+                
+                jcodemodel.build(new File("/home/oem/Desktop/UI"));                
+            }
         }
         catch(Exception exception)
         {
-            
+            exception.printStackTrace();
         }
         
         return null;
--- src/apml/ui/compilers/java/builder/Jcmjmenuitembuilder.java
+++ src/apml/ui/compilers/java/builder/Jcmjmenuitembuilder.java
@@ -57,79 +57,148 @@ public class Jcmjmenuitembuilder
                 
                 JPackage jpackage = jcodemodel._package("org.widgets");                
                 
-                JDefinedClass jdefinedclass = jpackage._class("JMenuItem_"+String.format("%1$03d",i));     
+                JDefinedClass jdefinedclass = jpackage._class("JMenuItem_"+String.format("%1$03d",i));   
                 
                 jdefinedclass._extends(JMenuItem.class);
                 
-                JMethod constructor = jdefinedclass.constructor(JMod.PUBLIC);                               
+                JMethod constructor = jdefinedclass.constructor(JMod.PUBLIC);      
                 
-                try{constructor.body().directStatement("this.setAutoscrolls("+xml.getAttribute("setAutoscrolls")+");");                 }catch(Exception e){}
+                if(xml.getAttribute("setAutoscrolls")!=null && xml.getAttribute("setAutoscrolls").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setAutoscrolls("+xml.getAttribute("setAutoscrolls")+");");                 }catch(Exception e){}               
+                }
                 
-                try{constructor.body().directStatement("this.setAlignmentY("+xml.getAttribute("setAlignmentY")+");");                   }catch(Exception e){}
+                if(xml.getAttribute("setAlignmentY")!=null && xml.getAttribute("setAlignmentY").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setAlignmentY("+xml.getAttribute("setAlignmentY")+");");                   }catch(Exception e){}                
+                }
                 
-                try{constructor.body().directStatement("this.setAlignmentX("+xml.getAttribute("setAlignmentX")+");");                   }catch(Exception e){}                
+                if(xml.getAttribute("setAlignmentX")!=null && xml.getAttribute("setAlignmentX").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setAlignmentX("+xml.getAttribute("setAlignmentX")+");");                   }catch(Exception e){}                
+                }
                 
-                try{constructor.body().directStatement("this.setBorderPainted("+xml.getAttribute("setBorderPainted")+");");             }catch(Exception e){}
+                if(xml.getAttribute("setBorderPainted")!=null && xml.getAttribute("setBorderPainted").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setBorderPainted("+xml.getAttribute("setBorderPainted")+");");             }catch(Exception e){}               
+                }
+                                
+                if(xml.getAttribute("setBounds")!=null && xml.getAttribute("setBounds").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setBounds("+xml.getAttribute("setBounds")+");");                           }catch(Exception e){}
+                }
+                                
+                if(xml.getAttribute("setBorder")!=null && xml.getAttribute("setBorder").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setBorder("+xml.getAttribute("setBorder")+");");                           }catch(Exception e){}                    
+                }
                 
-                try{constructor.body().directStatement("this.setBounds("+xml.getAttribute("setBounds")+");");                           }catch(Exception e){}
+                if(xml.getAttribute("setBackground")!=null && xml.getAttribute("setBackground").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setBackground("+xml.getAttribute("setBackground")+");");                   }catch(Exception e){}                                    
+                }
                 
-                try{constructor.body().directStatement("this.setBorder("+xml.getAttribute("setBorder")+");");                           }catch(Exception e){}
+                if(xml.getAttribute("setComponentPopupMenu")!=null && xml.getAttribute("setComponentPopupMenu").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setComponentPopupMenu("+xml.getAttribute("setComponentPopupMenu")+");");   }catch(Exception e){}                      
+                }
                 
-                try{constructor.body().directStatement("this.setBackground("+xml.getAttribute("setBackground")+");");                   }catch(Exception e){}                
-                
-                try{constructor.body().directStatement("this.setComponentPopupMenu("+xml.getAttribute("setComponentPopupMenu")+");");   }catch(Exception e){}  
-                
-                try{constructor.body().directStatement("this.setDisabledIcon("+xml.getAttribute("setDisabledIcon")+");");               }catch(Exception e){}
+                if(xml.getAttribute("setDisabledIcon")!=null && xml.getAttribute("setDisabledIcon").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setDisabledIcon("+xml.getAttribute("setDisabledIcon")+");");               }catch(Exception e){}
+                }
 
-                try{constructor.body().directStatement("this.setEnabled("+xml.getAttribute("setEnabled")+");");                         }catch(Exception e){}  
+                if(xml.getAttribute("setEnabled")!=null && xml.getAttribute("setEnabled").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setEnabled("+xml.getAttribute("setEnabled")+");");                         }catch(Exception e){}  
+                }                
                 
-                try{constructor.body().directStatement("this.setForeground("+xml.getAttribute("setForeground")+");");                   }catch(Exception e){} 
+                if(xml.getAttribute("setForeground")!=null && xml.getAttribute("setForeground").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setForeground("+xml.getAttribute("setForeground")+");");                   }catch(Exception e){} 
+                }
 
-                try{constructor.body().directStatement("this.setIcon("+xml.getAttribute("setIcon")+")");                                }catch(Exception e){}
+                if(xml.getAttribute("setIcon")!=null && xml.getAttribute("setIcon").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setIcon("+xml.getAttribute("setIcon")+")");                                }catch(Exception e){}
+                }
 
-                try{constructor.body().directStatement("this.setLabel("+xml.getAttribute("setLabel")+")");                              }catch(Exception e){}
+                if(xml.getAttribute("setLabel")!=null && xml.getAttribute("setLabel").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setLabel("+xml.getAttribute("setLabel")+")");                              }catch(Exception e){}
+                }
                 
-                try{constructor.body().directStatement("this.setLayout("+xml.getAttribute("setLayout")+");");                           }catch(Exception e){}
+                if(xml.getAttribute("setLayout")!=null && xml.getAttribute("setLayout").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setLayout("+xml.getAttribute("setLayout")+");");                           }catch(Exception e){}
+                }
                 
-                try{constructor.body().directStatement("this.setLocation("+xml.getAttribute("setLocation")+");");                       }catch(Exception e){}
+                if(xml.getAttribute("setLocation")!=null && xml.getAttribute("setLocation").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setLocation("+xml.getAttribute("setLocation")+");");                       }catch(Exception e){}
+                }
                 
-                try{constructor.body().directStatement("this.setMargin("+xml.getAttribute("setMargin")+");");                           }catch(Exception e){}
+                if(xml.getAttribute("setMargin")!=null && xml.getAttribute("setMargin").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setMargin("+xml.getAttribute("setMargin")+");");                           }catch(Exception e){}
+                }
                 
-                try{constructor.body().directStatement("this.setMinimumSize("+xml.getAttribute("setMinimumSize")+");");                 }catch(Exception e){}
+                if(xml.getAttribute("setMinimumSize")!=null && xml.getAttribute("setMinimumSize").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setMinimumSize("+xml.getAttribute("setMinimumSize")+");");                 }catch(Exception e){}
+                }
                 
-                try{constructor.body().directStatement("this.setMaximumSize("+xml.getAttribute("setMaximumSize")+");");                 }catch(Exception e){}            
+                if(xml.getAttribute("setMaximumSize")!=null && xml.getAttribute("setMaximumSize").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setMaximumSize("+xml.getAttribute("setMaximumSize")+");");                 }catch(Exception e){}            
+                }
                 
-                try{constructor.body().directStatement("this.setModel("+xml.getAttribute("setModel")+");");                             }catch(Exception e){}
+                if(xml.getAttribute("setModel")!=null && xml.getAttribute("setModel").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setModel("+xml.getAttribute("setModel")+");");                             }catch(Exception e){}
+                }
+                                
+                if(xml.getAttribute("setName")!=null && xml.getAttribute("setName").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setName("+xml.getAttribute("setName")+");");                               }catch(Exception e){}
+                }
                 
-                try{constructor.body().directStatement("this.setName("+xml.getAttribute("setName")+");");                               }catch(Exception e){}
+                if(xml.getAttribute("setPressedIcon")!=null && xml.getAttribute("setPressedIcon").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setPressedIcon("+xml.getAttribute("setPressedIcon")+");");                 }catch(Exception e){}
+                }
                 
-                try{constructor.body().directStatement("this.setPressedIcon("+xml.getAttribute("setPressedIcon")+");");                 }catch(Exception e){}
-                
-                try{constructor.body().directStatement("this.setRolloverIcon("+xml.getAttribute("setRolloverIcon")+");");               }catch(Exception e){}                    
+                if(xml.getAttribute("setRolloverIcon")!=null && xml.getAttribute("setRolloverIcon").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setRolloverIcon("+xml.getAttribute("setRolloverIcon")+");");               }catch(Exception e){}                    
+                }
 
-                try{constructor.body().directStatement("this.setText(\""+xml.getAttribute("setText")+"\");");                               }catch(Exception e){}   
+                if(xml.getAttribute("setText")!=null && xml.getAttribute("setText").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setText(\""+xml.getAttribute("setText")+"\");");                           }catch(Exception e){}   
+                }
                 
-                try{constructor.body().directStatement("this.setSize("+xml.getAttribute("setSize")+");");                               }catch(Exception e){}
+                if(xml.getAttribute("setSize")!=null && xml.getAttribute("setSize").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setSize("+xml.getAttribute("setSize")+");");                               }catch(Exception e){}
+                }
                 
-                try{constructor.body().directStatement("this.setToolTipText("+xml.getAttribute("setToolTipText")+");");                 }catch(Exception e){}
+                if(xml.getAttribute("setToolTipText")!=null && xml.getAttribute("setToolTipText").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setToolTipText("+xml.getAttribute("setToolTipText")+");");                 }catch(Exception e){}
+                }
                 
-                try{constructor.body().directStatement("this.setVisible("+xml.getAttribute("setVisible")+");");                         }catch(Exception e){}
+                if(xml.getAttribute("setVisible")!=null && xml.getAttribute("setVisible").length()>0)
+                {
+                    try{constructor.body().directStatement("this.setVisible("+xml.getAttribute("setVisible")+");");                         }catch(Exception e){}
+                }
+                
+                jcodemodel.build(new File("/home/oem/Desktop/UI"));                
             }
         }
         catch(Exception exception)
         {
             exception.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
-                jcodemodel.build(new File("/home/oem/Desktop/UI"));
-            }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
         }
         
         return null;
