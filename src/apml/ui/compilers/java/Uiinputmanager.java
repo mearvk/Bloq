250e367900c44a243481681ed8edc749675e1b88
Max Rupplin - Sr. Scientologist <mearvk@outlook.com>
2017-03-17 19:56:56 -0400

    latest

--- src/apml/ui/compilers/java/Uiinputmanager.java
+++ src/apml/ui/compilers/java/Uiinputmanager.java
@@ -131,7 +131,7 @@ import javax.swing.JToolTip;
 import javax.swing.JTree;
 
 /**
- * @author max rupplin
+ * @author Max Rupplin
  */
 public class Uiinputmanager
 {    
@@ -156,24 +156,42 @@ public class Uiinputmanager
     {            
         switch(tagname)
         {
-            case "//jfilechooser":
-                this.jcmmodels.jfilechoosermodels.addAll(new Jcmjfilechooserbuilder(this.apml, tagname, JFileChooser.class).builder.build()); break;
-            
+            case "//frame": 
+                this.jcmmodels.framemodels.addAll(new Jcmframebuilder(this.apml, tagname, Frame.class).builder.build()); break;             
+
+            case "//japplet": 
+                this.jcmmodels.jappletmodels.addAll(new Jcmjappletbuilder(apml, tagname, JApplet.class).builder.build()); break;                
+                
             case "//jbutton": 
-                this.jcmmodels.jbuttonmodels.addAll(new Jcmjbuttonbuilder(this.apml, tagname, JButton.class).builder.build()); break;
+                this.jcmmodels.jbuttonmodels.addAll(new Jcmjbuttonbuilder(this.apml, tagname, JButton.class).builder.build()); break;            
+
+            case "//jcheckbox": 
+                this.jcmmodels.jcheckboxmodels.addAll(new Jcmjcheckboxbuilder(this.apml, tagname, JCheckBox.class).builder.build()); break;                 
+
+            case "//jcolorchooser": 
+                this.jcmmodels.jcolorchoosermodels.addAll(new Jcmjcolorchooserbuilder(apml, tagname, JColorChooser.class).builder.build()); break;                
+                
+            case "//jcombobox": 
+                this.jcmmodels.jcomboboxmodels.addAll(new Jcmjcomboboxbuilder(this.apml, tagname, JComboBox.class).builder.build()); break; 
+
+            case "//jeditorpane": 
+                this.jcmmodels.jeditorpanemodels.addAll(new Jcmjeditorpanebuilder(apml, tagname, JEditorPane.class).builder.build()); break;
+                
+            case "//jfilechooser":
+                this.jcmmodels.jfilechoosermodels.addAll(new Jcmjfilechooserbuilder(this.apml, tagname, JFileChooser.class).builder.build()); break;            
 
             case "//jframe": 
                 this.jcmmodels.jframemodels.addAll(new Jcmjframebuilder(this.apml, tagname, JFrame.class).builder.build()); break;
 
-            case "//frame": 
-                this.jcmmodels.framemodels.addAll(new Jcmframebuilder(this.apml, tagname, Frame.class).builder.build()); break; 
-
-            case "//jcheckbox": 
-                this.jcmmodels.jcheckboxmodels.addAll(new Jcmjcheckboxbuilder(this.apml, tagname, JCheckBox.class).builder.build()); break; 
-
-            case "//jcombobox": 
-                this.jcmmodels.jcomboboxmodels.addAll(new Jcmjcomboboxbuilder(this.apml, tagname, JComboBox.class).builder.build()); break; 
-           
+            case "//jinternalframe": 
+                this.jcmmodels.jinternalframemodels.addAll(new Jcmjinternalframebuilder(apml, tagname, JInternalFrame.class).builder.build()); break;                
+                
+            case "//jlabel": 
+                this.jcmmodels.jlabelmodels.addAll(new Jcmjlabelbuilder(apml, tagname, JLabel.class).builder.build()); break;                
+                
+            case "//jlayeredpane": 
+                this.jcmmodels.jlayeredpanemodels.addAll(new Jcmjlayeredpanebuilder(apml, tagname, JLayeredPane.class).builder.build()); break;                
+                
             case "//jlist": 
                 this.jcmmodels.jlistmodels.addAll(new Jcmjlistbuilder(this.apml, tagname, JList.class).builder.build()); break;
 
@@ -186,26 +204,41 @@ public class Uiinputmanager
             case "//jmenuitem": 
                 this.jcmmodels.jmenuitemmodels.addAll(new Jcmjmenuitembuilder(this.apml, tagname, JMenuItem.class).builder.build()); break;
 
+            case "//jpanel": 
+                this.jcmmodels.jpanelmodels.addAll(new Jcmjpanelbuilder(apml, tagname, JPanel.class).builder.build()); break;                                
+
+            case "//jpasswordfield": 
+                this.jcmmodels.jpasswordfieldmodels.addAll(new Jcmjpasswordfieldbuilder(apml, tagname, JPasswordField.class).builder.build()); break;
+                
+            case "//jprogressbar": 
+                this.jcmmodels.jprogressbarmodels.addAll(new Jcmjprogressbarbuilder(apml, tagname, JProgressBar.class).builder.build()); break;                
+                
             case "//jradiobutton": 
                 this.jcmmodels.jradiobuttonmodels.addAll(new Jcmjradiobuttonbuilder(this.apml, tagname, JRadioButton.class).builder.build()); break; 
 
+            case "//jrootpane": 
+                this.jcmmodels.jrootpanemodels.addAll(new Jcmjrootpanebuilder(apml, tagname, JRootPane.class).builder.build()); break;                   
+                
+            case "//jscrollpane": 
+                this.jcmmodels.jscrollpanemodels.addAll(new Jcmjscrollpanebuilder(apml, tagname, JScrollPane.class).builder.build()); break;                
+                
+            case "//jseparator": 
+                this.jcmmodels.jseparatormodels.addAll(new Jcmjseparatorbuilder(apml, tagname, JSeparator.class).builder.build()); break;                
+                
             case "//jslider": 
                 this.jcmmodels.jslidermodels.addAll(new Jcmjsliderbuilder(this.apml, tagname, JSlider.class).builder.build()); break;
 
             case "//jspinner": 
                 this.jcmmodels.jspinnermodels.addAll(new Jcmjspinnerbuilder(apml, tagname, JSpinner.class).builder.build()); break;
 
+            case "//jsplitpane": 
+                this.jcmmodels.jsplitpanemodels.addAll(new Jcmjsplitpanebuilder(apml, tagname, JSplitPane.class).builder.build()); break;                
+
+            case "//jtabbedpane": 
+                this.jcmmodels.jtabbedpanemodels.addAll(new Jcmjtabbedpanebuilder(apml, tagname, JTabbedPane.class).builder.build()); break;
+                
             case "//jtextfield": 
-                this.jcmmodels.jtextfieldmodels.addAll(new Jcmjtextfieldbuilder(apml, tagname, JTextField.class).builder.build()); break; 
-
-            case "//jpasswordfield": 
-                this.jcmmodels.jpasswordfieldmodels.addAll(new Jcmjpasswordfieldbuilder(apml, tagname, JPasswordField.class).builder.build()); break;
-
-            case "//jcolorchooser": 
-                this.jcmmodels.jcolorchoosermodels.addAll(new Jcmjcolorchooserbuilder(apml, tagname, JColorChooser.class).builder.build()); break;
-
-            case "//jeditorpane": 
-                this.jcmmodels.jeditorpanemodels.addAll(new Jcmjeditorpanebuilder(apml, tagname, JEditorPane.class).builder.build()); break;
+                this.jcmmodels.jtextfieldmodels.addAll(new Jcmjtextfieldbuilder(apml, tagname, JTextField.class).builder.build()); break;               
 
             case "//jtextpane": 
                 this.jcmmodels.jtextpanemodels.addAll(new Jcmjtextpanebuilder(apml, tagname, JTextPane.class).builder.build()); break; 
@@ -213,44 +246,11 @@ public class Uiinputmanager
             case "//jtree": 
                 this.jcmmodels.jtreemodels.addAll(new Jcmjtreebuilder(apml, tagname, JTree.class).builder.build()); break; 
 
-            case "//jlabel": 
-                this.jcmmodels.jlabelmodels.addAll(new Jcmjlabelbuilder(apml, tagname, JLabel.class).builder.build()); break;
-
-            case "//jprogressbar": 
-                this.jcmmodels.jprogressbarmodels.addAll(new Jcmjprogressbarbuilder(apml, tagname, JProgressBar.class).builder.build()); break;
-
-            case "//jseparator": 
-                this.jcmmodels.jseparatormodels.addAll(new Jcmjseparatorbuilder(apml, tagname, JSeparator.class).builder.build()); break;
-
+            case "//jtoolbar": 
+                this.jcmmodels.jtoolbarmodels.addAll(new Jcmjtoolbarbuilder(apml, tagname, JToolBar.class).builder.build()); break;                
+                
             case "//jtooltip": 
                 this.jcmmodels.jtooltipmodels.addAll(new Jcmjtooltipbuilder(apml, tagname, JToolTip.class).builder.build()); break;
-
-            case "//japplet": 
-                this.jcmmodels.jappletmodels.addAll(new Jcmjappletbuilder(apml, tagname, JApplet.class).builder.build()); break;
-
-            case "//jpanel": 
-                this.jcmmodels.jpanelmodels.addAll(new Jcmjpanelbuilder(apml, tagname, JPanel.class).builder.build()); break;
-
-            case "//jscrollpane": 
-                this.jcmmodels.jscrollpanemodels.addAll(new Jcmjscrollpanebuilder(apml, tagname, JScrollPane.class).builder.build()); break;
-
-            case "//jsplitpane": 
-                this.jcmmodels.jsplitpanemodels.addAll(new Jcmjsplitpanebuilder(apml, tagname, JSplitPane.class).builder.build()); break;
-
-            case "//jtabbedpane": 
-                this.jcmmodels.jtabbedpanemodels.addAll(new Jcmjtabbedpanebuilder(apml, tagname, JTabbedPane.class).builder.build()); break;
-
-            case "//jtoolbar": 
-                this.jcmmodels.jtoolbarmodels.addAll(new Jcmjtoolbarbuilder(apml, tagname, JToolBar.class).builder.build()); break;
-
-            case "//jinternalframe": 
-                this.jcmmodels.jinternalframemodels.addAll(new Jcmjinternalframebuilder(apml, tagname, JInternalFrame.class).builder.build()); break;
-
-            case "//jlayeredpane": 
-                this.jcmmodels.jlayeredpanemodels.addAll(new Jcmjlayeredpanebuilder(apml, tagname, JLayeredPane.class).builder.build()); break;
-
-            case "//jrootpane": 
-                this.jcmmodels.jrootpanemodels.addAll(new Jcmjrootpanebuilder(apml, tagname, JRootPane.class).builder.build()); break;   
         }        
         
         return true;
